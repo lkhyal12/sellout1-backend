0e9dd3e4fc4ffe4e5b208e1d63850e0b0cc33cdb
lkhyal <lkhyal.ab@gmail.com>
2026-07-24 03:56:52 +0200

    changing mongoose connect order

--- server.js
+++ server.js
@@ -24,9 +24,16 @@ app.use("/api/products", productsRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/checkout", paymentRouter);
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("connected to mongodb"))
-    .catch((err) => console.log(err));
-});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err);
+  });
